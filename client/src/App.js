c4b5ab7d816c009eea1470b7834e84945250594e
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-29 12:09:11 -0500

    MVP Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            ghUser: {},
+            followers: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/users/tristangrovender")
+            .then(res => res.json())
+            .then(user => {
+                console.log(user);
+                this.setState({ ghUser: user });
+            })
+            .catch(err => console.log("Error fetching data"));
+        fetch("https://api.github.com/users/tristangrovender/followers")
+            .then(res => res.json())
+            .then(associates => {
+                console.log(associates);
+                this.setState({ followers: associates });
+            })
+            .catch(err => console.log("Error fetching data"));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Github user:</h1>
+                <img
+                    width="100"
+                    height="100"
+                    src={this.state.ghUser.avatar_url}
+                    key={this.state.ghUser.id}
+                    alt={this.state.ghUser.name}
+                />
+                <h2>Followers:</h2>
+                {this.state.followers.map(associate => (
+                    <img
+                        width="100"
+                        height="100"
+                        src={associate.avatar_url}
+                        key={associate.id}
+                        alt={associate.name}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
